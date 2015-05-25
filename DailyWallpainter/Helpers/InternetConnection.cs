e465e5a6002173ca779cacd11380099003560ffb
xail <iamxail@gmail.com>
2015-05-25 16:25:26 +0900

    Add waiting for internet connection without termination

---
+++ DailyWallpainter/Helpers/InternetConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace DailyWallpainter.Helpers
+{
+    public static class InternetConnection
+    {
+        //http://stackoverflow.com/questions/20309158/c-sharp-checking-internet-connection
+        public static bool IsAvailable()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (var stream = client.OpenRead("http://www.google.com"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
